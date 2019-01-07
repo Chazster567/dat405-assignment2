87ad186a6634b49c252da8475cf1aa25c7489dcc
Chazster567 <charles.whitehair@students.plymouth.ac.uk>
2019-01-07 19:30:38 +0000

    Main updates

--- dataVisualisation/datasketch.js
+++ dataVisualisation/datasketch.js
@@ -7,7 +7,8 @@ let music;
 let i;
 let input;
 let button;
-let artist = 'beatles';
+let artist = 'oasis';
+var angle = 0;
 
 //The preload function is executed before initializing the code in setup
 //Loads any related data or media files
@@ -22,14 +23,11 @@ function preload() {
 
 function setup() {
   console.log(music); //Return all JSON data
-  console.log(music.toptracks.track[0].listeners);
-  console.log(music.toptracks.track[0].name);
   //Display temperature information on the screen
-  let tcol = random(150,255)
   createCanvas(1280, 720);
   background(0);
   input = createInput();
-  input.position(570, 350);
+  input.position(560, 350);
 
   button = createButton('Submit');
   button.position(620, 370);
@@ -39,45 +37,33 @@ function setup() {
 
 function query() {
   artist = input.value();
-  console.log(artist)
-  preload()
+  console.log(artist);
 }
 
 function draw() {
-  i = round(random(40))
   // Sun
   fill(255, 255, 0);
   ellipse(640, 360, 200, 200);
 
-  // Mercury
-  fill(200);
-  ellipse(1020, 360, music.toptracks.track[i].listeners/10000, music.toptracks.track[i].listeners/10000);
-
+  for (let x = 800; x <= width;) {
+    i = round(random(40))
+    let planetSize = music.toptracks.track[i].listeners/10000
+    fill(random(120,255), random(120,255), random(120,255));
+    ellipse(x, 360, planetSize, planetSize);
+    textSize(12);
+    textAlign(CENTER);
+    text(music.toptracks.track[i].name, x, 375 + planetSize/2);
+    text(music.toptracks.track[i].listeners + ' plays', x, 390 + planetSize/2);
+  }
   // Venus
-  fill(255, 130, 0);
-  ellipse(860, 360, music.toptracks.track[i].listeners/10000, music.toptracks.track[i].listeners/10000);
+  //fill(255, 130, 0);
+  //ellipse(860, 360, music.toptracks.track[i].listeners/3000, music.toptracks.track[i].listeners/3000);
 
   // Earth
-  fill(18, 154, 221);
-  ellipse(900, 360, music.toptracks.track[i].listeners/10000, music.toptracks.track[i].listeners/10000);
+  //fill(18, 154, 221);
+  //ellipse(900, 360, music.toptracks.track[i].listeners/3000, music.toptracks.track[i].listeners/3000);
 
   // Mars
-  fill(255, 57, 4);
-  ellipse(1040, 360, music.toptracks.track[i].listeners/10000, music.toptracks.track[i].listeners/10000);
-
-  // Jupiter
-  fill(255, 130, 0);
-  ellipse(1180, 360, music.toptracks.track[i].listeners/10000, music.toptracks.track[i].listeners/10000);
-
-  // Saturn
-  fill(255, 235, 176);
-  ellipse(1200, 360, music.toptracks.track[i].listeners/10000, music.toptracks.track[i].listeners/10000);
-
-  // Uranus
-  fill(38, 222, 255);
-  ellipse(850, 360, music.toptracks.track[i].listeners/10000, music.toptracks.track[i].listeners/10000);
-
-  // Neptune
-  fill(0, 150, 255);
-  ellipse(950, 360, music.toptracks.track[i].listeners/10000, music.toptracks.track[i].listeners/10000);
+  //fill(255, 57, 4);
+  //ellipse(1040, 360, music.toptracks.track[i].listeners/3000, music.toptracks.track[i].listeners/3000);
 }
